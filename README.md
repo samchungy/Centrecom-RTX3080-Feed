f611ed4555fb1cefae80fa626474512cd70beef9
Sam Chung <schung2@deloitte.com.au>
2020-08-05 22:04:08 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Serverless AFR RSS Feed
+
+A simple 30 minute serverless RSS Feed designed to be invoked by Slack's RSS Feed.
+
+## Description
+
+This bot is designed to scrape Google Web Search results for afr.com articles with the keyword "Deloitte" in the title and return them in a RSS Feed which will be consumed by Slack's Feed app.
+
+## Getting Started
+
+### Dependencies
+
+* serverless
+* axios
+* feed
+
+### Installing
+
+* Create a [Google Custom Search](https://programmablesearchengine.google.com/), grab it's cx value.
+* Create a [Google API Key](https://console.developers.google.com/apis/credentials) for Custom Search.
+* Create an `.env` file and fill them with `GOOGLE_API_KEY` and `GOOGLE_CUSTOM_SEARCH_CX` environment variables.
+* Run `npm install --prod` to install dependencies.
+* Run `sls offline` to run locally.
+* Run `sls deploy` to deploy to AWS.
+* On Slack run `/feed subscribe {your-app-url}`
---
+++ feed2.js
@@ -0,0 +1,102 @@
+'use strict';
+const {default: axios} = require('axios');
+const cheerio = require('cheerio');
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+const dynamo = () => {
+  const PRODUCT = 'rtx3080';
+
+  const dynamodb = new DynamoDB.DocumentClient(
+    process.env.IS_OFFLINE ? {
+      endpoint: 'http://localhost:8000',
+      region: 'localhost',
+      accessKeyId: 'DEFAULT_ACCESS_KEY',
+      secretAccessKey: 'DEFAULT_SECRET',
+    } : {},
+  );
+
+  const loadProducts = async () => {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        [process.env.PK]: PRODUCT
+      }
+    }
+   return dynamodb.get(params).promise();
+  }
+
+  const updateProducts = async (products) => {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        [process.env.PK]: PRODUCT,
+        items: products
+      }
+    }
+    return dynamodb.put(params).promise();
+  }
+
+  return {
+    loadProducts,
+    updateProducts
+  }
+}
+
+module.exports.handler = async event => {
+  const db = dynamo();
+  const agent = axios.create({
+    baseURL: "https://www.centrecom.com.au/nvidia-2?orderby=11&pagesize=16"
+  });
+
+  const res = await agent.get('/').catch(error => {
+    console.error(error);
+    throw error;
+  });
+
+  const $ = cheerio.load(res.data);
+
+  const PRODUCT_DIV_CLASS = '.prbox_link';
+  const PRODUCT_NAME_DIV_CLASS = '.prbox_name';
+  const PRODUCT_PRICE_DIV_CLASS = '.saleprice';
+  const PRODUCT_IN_STOCK = '.prbox_green';
+
+  const productsLoad = await db.loadProducts();
+  const products = (productsLoad.Item && productsLoad.Item.items) || [];
+  const productResults = [];
+  const newProducts = [];
+
+
+  $(PRODUCT_DIV_CLASS).each((i, e) => {
+    const href = e.attribs.href;
+    const name = $(e).find(PRODUCT_NAME_DIV_CLASS).text();
+    const price = $(e).find(PRODUCT_PRICE_DIV_CLASS).text();
+    const instock = $(e).find(PRODUCT_IN_STOCK).text();
+    if (href.includes('3080') && instock){
+      const product = products.find(p => p.href === href);
+      if (!product){
+        newProducts.push({
+          url: 'https://www.centrecom.com.au' + e.attribs.href,
+          online: instock.includes('Available online'),
+          name,
+          price
+        })
+      }
+      productResults.push({
+        href,
+      });
+    }
+  });
+
+  
+  await Promise.all(newProducts.map(async (p) => {
+    const stockLocation = p.online ? 'Online' : 'Instore';
+    const text = `${stockLocation}: ${p.name} - ${p.price}` + '\n' + p.url
+    return axios.post(process.env.SLACK_URL, { text });
+  }));
+
+  if(products.length !== productResults.length || productResults.some(pr => !products.find(p => pr.href === p.href))){
+    await db.updateProducts(productResults);
+  }
+
+  return {}
+};
